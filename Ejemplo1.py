a8a1e452ba4e9ed0ba6bdd9a183d2ba59c838119
Juan Carlos Rodriguez <juan.rodriguez@lahuerta.tecmm.edu.mx>
2023-06-05 16:46:48 -0600

    Tercer version

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,3 +1,10 @@
 print("Hola mundo, version 1.0")
-for i in range (1,10):
+n= input("Ingrese un numero menor a 10")
+n = int(n)
+for i in range (1,n):
     print("El valor es",i)
+    
+    if i % 2==0:
+         print("El numero es par ")
+    else:
+        print("El numero es impar")
