811872b69dd5d85986bd90eaa6425e76ac6a6c5b
Juan Carlos Rodriguez <juan.rodriguez@lahuerta.tecmm.edu.mx>
2023-06-05 21:53:59 -0600

    Proyecto ejemplo Html y Css

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,10 +1,8 @@
 print("Hola mundo, version 1.0")
-n= input("Ingrese un numero menor a 10")
+n= input("Ingrese un numero menor a 10;  ")
 n = int(n)
 for i in range (1,n):
-    print("El valor es",i)
-    
     if i % 2==0:
-         print("El numero es par ")
+         print("El numero ", i, "es par ")
     else:
-        print("El numero es impar")
+        print("El numero ", i, " es impar")
